c02bb5ef9025866d87d939c553d09091406668e1
elo <eloisemecozzi@gmail.com>
2017-12-04 11:24:26 +0100

    ajout texte avec cssindex.php

--- index.php
+++ index.php
@@ -3,13 +3,34 @@
 <!-- Wrapper sections -->
 <div class="content">
   <section id="mysect1">
-    <h1>Présentation (slide ou vidéo en fond ?)</h1>
+    <h1>Présentation (slide avec photos)</h1>
   </section>
   <section id="mysect2">
-    <h1>SECTION 2 (petit texte)</h1>
+    <div class="presentation">
+      <h2>﻿Vous cherchez un groupe pour animer un mariage, une réception, une soirée ?</h2>
+      <p>Nous sommes SUNFLOWER, un duo guitare-voix, et nous possédons un large répertoire de musiques dansantes et d'ambiance. Nous vous proposons un cocktail de reggae, rock, soul en français et anglais (Bob marley, Sting ,Selah Sue, Police, Beatles....).</p>
+      <p>Il vous suffira de faire appel à nous, nous ferons de cet soirée un événement inoubliable.</p>
+      <p>n'hésitez pas à nous contacter et nous vous enverrons une démo musicale.</p>
+      <p>Nous sommes un duo guitare voix et notre style tourne autour du reggae de la soul avec quelques reprises rock.</p>
+      <p>Contact : 06 10 93 37 46</p>
+      <p>Style :
+        <ul>
+          <li>Reggae soul</li>
+          <li>Influences</li>
+          <li>Bob marley, Sting, Beatles</li>
+        </ul>
+        </p>
+        <p>Composition :
+        <ul>
+          <li>Alban à la guitare et les choeurs</li>
+          <li>Carole chanteuse principal</li>
+          <li>Looper plus percussions</li>
+        </ul>
+      </p>
+    </div>
   </section>
   <section id="mysect3">
-    <h1>SECTION 3</h1>
+    <iframe src="https://player.vimeo.com/video/2306191?autoplay=1&title=0&byline=0&portrait=0" width="840" height="473" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
   </section>
   <section id="mysect4">
     <h1>SECTION 4</h1>
